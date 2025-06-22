f28b23c131bad1993c68d19e26c3dc7debd71911
sonakshi-raj <sonakshisunil2004@gmail.com>
2025-06-22 12:25:21 +0530

    Add User model

---
+++ client/models/userModels.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    rollNumber: {
+        type: Number,
+        required: [true, "Please provide a roll number"],
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: [true, "Please provide a name"],
+    },
+    email: {
+        type: Number,
+        required: [true, "Please provide a email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+
+    }
+});
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+export default User;
