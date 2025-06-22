64156f3f9af16e2be0850440b8a9e084ab1f5b4e
sonakshi-raj <sonakshisunil2004@gmail.com>
2025-06-22 15:06:12 +0530

    Implement login functionality with user validation

---
+++ client/app/api/login/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import User from "@/models/userModels";
+import bcrypt from "bcryptjs";
+import { connect } from "@/DBConfig/DBConfig";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { username, password } = reqBody;
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exist." },
+        { status: 400 }
+      );
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return NextResponse.json(
+        { error: "Check your credentials" },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json({
+      message: "Logged In successfully.",
+      success: true,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  }
+     catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
