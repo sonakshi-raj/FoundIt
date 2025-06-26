aa6da04aa4c2e5362b3a6a1e15aa37bef1311c6a
sonakshi-raj <sonakshisunil2004@gmail.com>
2025-06-26 23:09:29 +0530

    Add profile page route

---
+++ client/app/api/profile/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import User from "@/models/userModels";
+import { connect } from "@/DBConfig/DBConfig";
+import { getDataFromToken } from "@/helper/getDataFromToken";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const userId = getDataFromToken(request);
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+    return NextResponse.json({
+      data: user,
+      status: true,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
