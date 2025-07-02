bce199632b66fac1532c4ca144bb7e9db38d05ac
sonakshi-raj <sonakshisunil2004@gmail.com>
2025-07-02 14:06:42 +0530

    Add item schema

---
+++ client/models/itemModel.ts
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema(
+  {
+    createdBy_user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    keywords: {
+      type: [String], 
+      default: [],
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium",
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    is_claimed: {
+      type: Boolean,
+      default: false,
+    },
+    uniqueQuestion: {
+      type: String,
+      required: true,
+    },
+    itemPicture: {
+      type: String, 
+      required: false,
+    },
+    createdDate: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+module.exports = mongoose.model("Item", itemSchema);
